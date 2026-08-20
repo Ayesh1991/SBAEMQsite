6302924d742e5611718ba847cff5c66b2d06aad4
Ayesh1991 <30837358+Ayesh1991@users.noreply.github.com>
2026-08-21 01:08:14 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    activation then drops every older cache.
    ============================================================ */
 
-const VERSION = 'aureum-v67';
+const VERSION = 'aureum-v68';
 const CORE = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', e => {
